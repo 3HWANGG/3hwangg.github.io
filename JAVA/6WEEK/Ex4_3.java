551c665a908b388f703e8b055127e1a024d8e72b
3HWANGG <108020431+3HWANGG@users.noreply.github.com>
2023-10-05 18:09:53 +0900

    Create Ex4_3.java

---
+++ JAVA/6WEEK/Ex4_3.java
@@ -0,0 +1,15 @@
+// 	Ex4_3 반복문) for문
+package org.javaro.lecture;
+
+public class Ex4_3 {
+
+	public static void main(String[] args) {
+		System.out.println("황은진"+"-"+"20221137"+"-"+"반복문");
+		int End = 15; // 종료 조건
+		for (int x = 11; x <=End; x++) {
+			System.out.println("x="+x+"End="+End+","+(x<=End));
+		}
+
+	}
+
+}
