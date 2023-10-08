a83c3bc1cda2348b9901688e75ad8f766512fe16
3HWANGG <108020431+3HWANGG@users.noreply.github.com>
2023-10-08 18:24:26 +0900

    Create Ex1_3.java

---
+++ JAVA/3WEEK/Ex1_3.java
@@ -0,0 +1,10 @@
+package org.javaro.lecture;
+import java.util.*;
+public class Ex1_3 {
+	public static void main(String[] args) {
+		System.out.println("황은진"+"-"+"20221137"+"-"+"변수");
+		int grade;
+		System.out.println("메소드내 로컬 변수는 초기화가 되지 않으면 에러 발생!!");
+	}
+
+}
