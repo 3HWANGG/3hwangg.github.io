f50d5fba14559ee934e52d0492e9b7067064dc25
3HWANGG <108020431+3HWANGG@users.noreply.github.com>
2023-10-08 18:22:04 +0900

    Create Ex1_1.java

---
+++ JAVA/3WEEK/Ex1_1.java
@@ -0,0 +1,13 @@
+package org.javaro.lecture;
+import java.util.*;
+public class Ex1_1 {
+	public static void main(String[] args) {
+		System.out.println("황은진"+"-"+"20221137"+"-"+"변수");
+		int grade = 4;
+		System.out.println(grade+"학년"+" "+"홍길동");	// 이름
+		System.out.println(grade+"학년"+" "+"성춘향");	// 이름
+		System.out.println(grade+"학년"+" "+"이몽룡");	// 이름
+		System.out.println(grade+"학년"+" "+"변학도");	// 이름
+	}
+
+}
