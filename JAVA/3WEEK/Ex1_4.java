faf2bab4055367908366f4f84549001a58c1a862
3HWANGG <108020431+3HWANGG@users.noreply.github.com>
2023-10-08 18:24:59 +0900

    Create Ex1_4.java

---
+++ JAVA/3WEEK/Ex1_4.java
@@ -0,0 +1,16 @@
+package org.javaro.lecture;
+import java.util.*;
+public class Ex1_4 {
+
+	public static void main(String[] args) {
+		System.out.println("황은진"+"-"+"20221137"+"-"+"변수");
+		int a = 10; // int 정수형, 이름:a, 10대입
+		int b = 20; // int 정수형, 이름:b, 20대입
+		int c;	//int 정수형, 이름:c, 초기화 없음
+		c = a+b; //c = a+b
+		System.out.println("변수 타입별 테스트");
+		System.out.println("포스트잇 a의 값"+a+"과"+"포스트잇 b의 값"+b+"의 합은 "+c+"입니다");
+		
+	}
+
+}
