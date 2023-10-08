cd4c306dd8b84ff094da2853a2cdff97ff1424ef
3HWANGG <108020431+3HWANGG@users.noreply.github.com>
2023-10-08 18:23:12 +0900

    Create Ex1_2.java

---
+++ JAVA/3WEEK/Ex1_2.java
@@ -0,0 +1,16 @@
+package org.javaro.lecture;
+import java.util.*;
+public class Ex1_2 {
+	static int intVar;
+	static long longVar;
+	static boolean booleanVar;
+	static double doubleVar;
+	static char charVar;
+	
+	public static void main(String[] args) {
+		System.out.println("황은진"+"-"+"20221137"+"-"+"변수");
+		System.out.println("클래스 멤버변수의 자동초기값을 확인하기 위해 변수설정 및 인쇄 -> 초기값 인쇄됨");
+		System.out.println(intVar+","+longVar+","+charVar+","+booleanVar+","+doubleVar);
+	}
+
+}
