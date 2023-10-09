be7942a2bd748c497bfa76b13445351635de52fd
3HWANGG <108020431+3HWANGG@users.noreply.github.com>
2023-10-10 00:36:12 +0900

    Create Ex2_1H.java

---
+++ JAVA/4WEEK/Ex2_1H.java
@@ -0,0 +1,16 @@
+package org.javaro.lecture;
+
+public class Ex2_1H {
+
+	public static void main(String[] args) {
+		System.out.println("황은진"+"-"+"20221137"+"-"+"연산자");
+		int secondVar = 1000;	// 입력 초
+		int minOut = secondVar / 60;	// 분 계산
+		int secondOut = secondVar % 60; // 남은 초 계산
+		System.out.println("분: "+minOut);
+		System.out.println("초: "+secondOut);
+		
+
+	}
+
+}
