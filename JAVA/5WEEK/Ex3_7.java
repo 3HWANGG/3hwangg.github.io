2bbad70b5dbf331a8e402d89c62dc23a7014c538
3HWANGG <108020431+3HWANGG@users.noreply.github.com>
2023-10-11 15:12:38 +0900

    Create Ex3_7.java

---
+++ JAVA/5WEEK/Ex3_7.java
@@ -0,0 +1,26 @@
+package org.javaro.lecture;
+
+public class Ex3_7 {
+
+	public static void main(String[] args) {
+		System.out.println("황은진"+"-"+"20221137"+"-"+"조건문");
+		// 계산식 (a%3)
+		int a = 10;
+		switch(a%3) {	// 나머지
+			case 1:
+				System.out.println("나머지는 1 입니다 ");
+				break;
+			case 2:
+				System.out.println("나머지는 2 입니다 ");
+				break;
+			default:
+				System.out.println("나머지는 0 입니다");
+				
+			
+		
+		
+		}
+
+	}
+
+}
