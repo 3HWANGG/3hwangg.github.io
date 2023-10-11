cc872e63102bef626e39601aa9767761f4c4287e
3HWANGG <108020431+3HWANGG@users.noreply.github.com>
2023-10-11 15:09:11 +0900

    Create Ex3_1.java

---
+++ JAVA/5WEEK/Ex3_1.java
@@ -0,0 +1,18 @@
+package org.javaro.lecture;
+
+public class Ex3_1 {
+
+	public static void main(String[] args) {
+		System.out.println("황은진"+"-"+"20221137"+"-"+"조건문");
+		int score = 95;	// 입력 변수화 가능
+		if(score >= 90)
+		{
+			System.out.println("A 학점");
+		}
+		else
+		{
+			System.out.println("A 학점이 아님");
+		}
+	}
+
+}
