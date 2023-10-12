7fee45207291c60a11c4e046a954f0ecfd14bc37
3HWANGG <108020431+3HWANGG@users.noreply.github.com>
2023-10-12 20:57:40 +0900

    Create Ex6_7.java

---
+++ JAVA/7WEEK/Ex6_7.java
@@ -0,0 +1,16 @@
+package org.javaro.lecture;
+
+public class Ex6_7 {
+
+	public static void main(String[] args) {
+		System.out.println("황은진"+"-"+"20221137"+"-"+"함수");
+		double x, y; // 값 1, 값2
+		
+		x = 10.0;
+		y = 20.0;
+		double sub_value = x-y;
+		System.out.println("빼기="+sub_value);
+
+	}
+
+}
