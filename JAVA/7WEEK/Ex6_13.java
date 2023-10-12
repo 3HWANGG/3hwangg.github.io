618e272a6872f89ce5d45a71fce3994c6e58bb2d
3HWANGG <108020431+3HWANGG@users.noreply.github.com>
2023-10-12 21:00:29 +0900

    Create Ex6_13.java

---
+++ JAVA/7WEEK/Ex6_13.java
@@ -0,0 +1,16 @@
+package org.javaro.lecture;
+
+public class Ex6_13 {
+	public static void main(String[] args) {
+		System.out.println("황은진"+"-"+"20221137"+"-"+"함수");
+		double x, y; // 밑변, 높이
+		
+		x = 10.0;
+		y = 20.0;
+		double area = 0.5*x*y;
+		System.out.println("면적="+area);
+		
+		
+	}
+
+}
