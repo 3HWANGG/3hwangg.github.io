fa39e8b95e5652ffc805217007f155ab40b5469a
3HWANGG <108020431+3HWANGG@users.noreply.github.com>
2023-10-12 20:59:30 +0900

    Create Ex6_9.java

---
+++ JAVA/7WEEK/Ex6_9.java
@@ -0,0 +1,15 @@
+package org.javaro.lecture;
+
+public class Ex6_9 {
+
+	public static void main(String[] args) {
+		System.out.println("황은진"+"-"+"20221137"+"-"+"함수");
+		double x, y; // 값 1, 값2
+		
+		x = 10.0;
+		y = 20.0;
+		double mult_value = x*y;
+		System.out.println("곱하기="+mult_value);
+	}
+
+}
