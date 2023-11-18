45eb54bd5535fac0940ecfd152fc2e7595284b37
3HWANGG <108020431+3HWANGG@users.noreply.github.com>
2023-11-18 22:00:50 +0900

    Create Ex10_4H.java

---
+++ JAVA/11WEEK/Ex10_4H.java
@@ -0,0 +1,19 @@
+// 과제 Ex10_4예제에 대하여  ArithmeticException 예외가 발생하도록 실행하고 결과 제출
+package org.javaro.lecture;
+public class Ex10_4H{
+	public static void main(String[] args) {
+		try {		// java  Ex10_3	10 20
+			System.out.println("매개변수로 받은 두개의 값");
+			int a = Integer.parseInt(args[0]);		// 콘솔 입력 문자열을 정수로 변환
+			int b = Integer.parseInt(args[1]);
+			System.out.println("a="+a+", b="+b );
+			System.out.println("a/b="+(a/b));
+			System.out.println("나누기 수행됨");
+		} catch(Exception e) {	// java  Ex10_3	10 
+			System.out.println("예외처리루틴:");
+			System.out.println(e+" 예외발생");
+		}finally {
+			System.out.println("예외처리후 finally 블럭 수행함");
+		}
+	}
+}
