74717de9ca8813dc1aaad4a205c7163ac21f6e2f
3HWANGG <108020431+3HWANGG@users.noreply.github.com>
2023-11-23 13:07:01 +0900

    Create Book.java

---
+++ JAVA/BOOKSTORE/Book.java
@@ -0,0 +1,42 @@
+package org.javaro.lecture;
+
+public class Book {
+	private String title;
+	private String author;
+	private Student student;
+	public Book(String title) {
+		this.title = title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getTitle() {
+		return this.title;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public  String getAuthor()
+	{
+		return this.author;
+	}
+	public void setStudent(Student student)
+	{
+		this.student = student;
+	}
+	public Student getStudent()
+	{
+		return this.student;
+	}
+	public String toString()
+	{
+		String available;		// 변수 available 설정
+		if(this.getStudent() == null) {		// book 객체의 student 객체가 null 이면
+			available = "대출가능";		// 대출 가능
+		}
+		else {	// null 이 아니면, 대출되었으면
+			available = "대출자="+this.getStudent().getName();		// available에 대출학생 이름 설정
+		}
+		return "제목="+this.getTitle()+",저자="+this.getAuthor()+","+available;	// 전체문구반환
+	}
+}
